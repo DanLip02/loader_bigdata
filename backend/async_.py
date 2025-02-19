1b25b84addfe6ccc4adc7283f795df94ffddfe41
danlipatov <danila.lipatov@ratings.ru>
2025-02-19 18:48:03 +0300

    add docker files +upd async_

--- backend/async_.py
+++ backend/async_.py
@@ -15,16 +15,15 @@ host = os.getenv("host")
 db = os.getenv("db")
 table = os.getenv("table")
 schema = os.getenv("schema")
+path = os.getenv("path")
 
 DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
-CSV_FILE_PATH = ""  # Убрал пробел
+CSV_FILE_PATH = path
 
-# Имя таблицы в PostgreSQL
 TABLE_NAME = table
 SCHEMA = schema
 
-# Явное указание типов
-DTYPE_MAP, dask_column_order = data_base.table_info(user, password, host, port, db, table)
+DTYPE_MAP, ORDER_ = data_base.table_info(user, password, host, port, db, table)
 
 async def load_csv_to_postgres():
     print("🔄 Подключаемся к базе данных...")
@@ -32,7 +31,7 @@ async def load_csv_to_postgres():
 
     print("📂 Загружаем CSV в Dask DataFrame...")
     ddf = dd.read_csv(CSV_FILE_PATH, dtype=DTYPE_MAP)
-    ddf.repartition(npartitions=200)
+    ddf = ddf.repartition(npartitions=200)
 
     missing_cols = {col: dtype for col, dtype in DTYPE_MAP.items() if col not in ddf.columns}
     print(missing_cols)
@@ -45,34 +44,42 @@ async def load_csv_to_postgres():
             ddf[col] = None
 
     ddf = ddf.astype(DTYPE_MAP)
-    ddf = ddf[dask_column_order]
+    ddf = ddf[ORDER_]
     print(len(ddf.columns))
-    columns = ", ".join(dask_column_order)
-    placeholders = ", ".join([f"${i+1}" for i in range(len(dask_column_order))])
+    columns = ", ".join(ORDER_)
+    placeholders = ", ".join([f"${i+1}" for i in range(len(ORDER_))])
     insert_query = f"INSERT INTO {SCHEMA}.{TABLE_NAME} ({columns}) VALUES ({placeholders})"
 
-    # Загружаем данные в PostgreSQL
     counter = 0
     for batch_df in ddf.to_delayed():
         counter += 1
         print(f"🚀 Загружаем batch #{counter}...")
 
-        # Вычисляем данные в фоне
         batch = await asyncio.to_thread(lambda: batch_df.compute())
 
-        print("stop_2")
-        # Заменяем NaN/NaT на None
         batch = batch.where(pd.notna(batch), 'null')
 
-        # Приводим данные к списку кортежей
         records = [tuple(row) for row in batch.itertuples(index=False, name=None)]
 
-        # Вставляем данные в PostgreSQL
-        await conn.executemany(insert_query, records)
+        # push Postgresql
 
-    # Закрываем соединение
+        await conn.executemany(insert_query, records)
+        # if counter == 46:
+        #     print(f"📌 Проверяем batch #{counter}")
+        #
+        #
+        #     batch = batch_df.compute()
+        #
+        #
+        #     print(batch.head())
+        #
+        #
+        #     print("Число столбцов в строках:", batch.apply(lambda row: len(row), axis=1).unique())
+        #     stop = 0
+        #
+
     await conn.close()
     print("✅ Данные успешно загружены!")
 
-# Запуск
+# start
 asyncio.run(load_csv_to_postgres())
